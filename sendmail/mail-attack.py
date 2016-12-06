9d5bd2c613278713670465276b061b9f1735a981
jyabek <oops.jybaek@gmail.com>
2016-12-07 08:35:57 +0900

    Fix comment error

--- sendmail/mail-attack.py
+++ sendmail/mail-attack.py
@@ -5,9 +5,9 @@ import smtplib
 from email.MIMEText import MIMEText
 from PyQt4 import QtGui, uic, QtCore
 
-'''
+"""
 sendmail examples: https://docs.python.org/2/library/email-examples.html
-'''
+"""
 
 class form(QtGui.QMainWindow):
     def __init__(self):
@@ -18,14 +18,15 @@ class form(QtGui.QMainWindow):
         self.connect(self.ui.pushButton, QtCore.SIGNAL("clicked()"), self.sendmail)
 
     def sendmail(self):
-'''
+
+        """
         print self.ui.form_mailserver.text()
         print self.ui.form_ehlo.text()
         print self.ui.form_mailfrom.text()
         print self.ui.form_rcptto.text()
         print self.ui.form_subject.text()
         print self.ui.form_data.toPlainText()
-'''
+        """
         msg = MIMEText(str(self.ui.form_data.toPlainText()), _charset='euc-kr')
         msg['Subject'] = str(self.ui.form_subject.text())
         msg['From'] = str(self.ui.form_mailfrom.text())
